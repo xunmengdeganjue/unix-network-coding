6a72303faef15b8e7f9aefe9aa5fe045519914ae
xunmengdeganjue <542873805@qq.com>
2016-06-22 13:34:37 +0800

    添加I/O复用相关测试代码

---
+++ chapter_6/client.c
@@ -0,0 +1,82 @@
+#include "new_unp.h"
+
+void str_cli(FILE *fp, int sockfd){
+ 	char sendline[MAXLINE], recvline[MAXLINE];
+ 	
+ 	while(Fgets(sendline, MAXLINE, fp) != NULL){
+ 		Writen(sockfd, sendline, strlen(sendline));
+ 		if(Readline(sockfd,recvline,MAXLINE) == 0)
+ 			err_quit("str_cli:server terminated prematurely");
+ 		Fputs(recvline, stdout);
+ 	}
+ }
+ 
+/*
+*    Function Name    : str_cli_twice
+*    Create Date      : 2016.06.15
+*    Author           : 
+*    Description      : 
+*    Param  Input     :			
+*    Return Code  1   : void
+*    revised History  :
+*/ 
+void str_cli_twice(FILE *fp, int sockfd){
+	
+	char sendline[MAXLINE], recvline[MAXLINE];
+	
+	while(Fgets(sendline, MAXLINE, fp) != NULL){
+		Writen(sockfd, sendline, strlen(sendline));
+		sleep(1);
+		Writen(sockfd, sendline, strlen(sendline)-1 );
+		if(Readline(sockfd,recvline,MAXLINE) == 0)
+			err_quit("str_cli:server terminated prematurely");
+		Fputs(recvline, stdout);
+	}
+	
+}
+void str_cli_select( FILE *fp, int sockfd){
+	int maxfdpl;
+	fd_set rset;
+	char sendline[MAXLINE], recvline[MAXLINE];
+	
+	FD_ZERO(&rset);
+	for(;;){
+			FD_SET(fileno(fp), &rset);
+			FD_SET(sockfd, &rset);
+			maxfdpl = max(fileno(fp), sockfd) +1;
+			Select(maxfdpl, &rset, NULL, NULL, NULL);
+			
+			if(FD_ISSET(sockfd, &rset)){ /*socket is readable*/
+				if(Readline(sockfd,recvline,MAXLINE) == 0 )
+					err_quit("str_cli:server terminated prematurely");
+				Fputs(recvline, stdout);
+			}
+			if(FD_ISSET(fileno(fp), &rset)){/*input is readable*/
+				if(Fgets(sendline, MAXLINE, fp) == NULL)
+					return ; 	/*all done*/
+				Writen(sockfd,sendline,strlen(sendline));			
+			}
+		
+	}
+}
+
+int main(int argc, char *argv[]){
+	int sockfd;
+	struct sockaddr_in servaddr;
+	
+	if(argc != 2)
+		err_quit("usage: tcpcli <IPaddress>");
+	
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	/*create the connection with the server.*/
+	Connect(sockfd, (SA *)&servaddr, sizeof(servaddr));
+	
+	str_cli_select(stdin,sockfd);
+	
+	exit(0);
+}
---
+++ chapter_6/server.c
@@ -0,0 +1,97 @@
+#include "new_unp.h"
+
+str_echo(int sockfd){
+	ssize_t n;
+	char buf[MAXLINE];
+again:
+	while((n = read(sockfd,buf,MAXLINE)) > 0)
+		Writen(sockfd,buf,n);
+	if(n<0&& errno == EINTR)
+		goto again;
+	else if(n < 0)
+		err_sys("str_echo:read error");
+}
+
+/*
+*    Function Name    : str_calc
+*    Create Date      : 2016.06.20
+*    Author           : 
+*    Description      : do the calculation of the data from the client.
+*    Param  Input     : sockfd.				
+*    Return Code  1   : void
+*    revised History  :
+*/
+void str_calc(int sockfd){
+	long arg1,arg2;
+	ssize_t n;
+	char line[MAXLINE];
+	
+	for(;;){
+		if((n =Readline(sockfd,line,MAXLINE)) == 0)
+			return;	/*connection closed by other end */
+		if(sscanf(line, "%ld%ld",&arg1,&arg2) == 2)
+			snprintf(line,sizeof(line),"%ld\n",arg1+arg2);
+		else
+			snprintf(line,sizeof(line),"input error\n");
+		n = strlen(line);
+		Writen(sockfd,line,n);
+	}
+}
+/*
+*    Function Name    : sig_chld
+*    Create Date      : 2016.06.15
+*    Author           : 
+*    Description      : just write data which from the client back to the client.
+*    Param  Input     : sockfd.				
+*    Return Code  1   : void
+*    revised History  :
+*/
+void sig_chld(int signo){
+	pid_t pid;
+	int stat;
+	
+	pid = wait(&stat);
+	printf("child %d terminated\n",pid);
+	return;
+}
+
+int main(int argc,char **argv){
+	int listenfd,connfd;
+	pid_t childpid;
+	socklen_t clilen;
+	struct sockaddr_in cliaddr,servaddr;
+	
+	listenfd = Socket(AF_INET,SOCK_STREAM, 0);
+	
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+	
+	Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+	
+	Listen(listenfd, LISTENQ);
+	
+	Signal(SIGCHLD, sig_chld); /*must call waitpid()*/
+	
+	for(;;){
+		clilen = sizeof(cliaddr);
+		if((connfd = accept(listenfd, (SA *) &(cliaddr),&clilen)) < 0){
+			if( errno == EINTR)
+				continue;
+			else
+				err_sys("accept error!");
+		}
+		
+		if((childpid = Fork()) == 0){ /*child process*/
+			Close(listenfd);		/*close listening socket*/	
+			str_echo(connfd);		/*process the request*/
+			//str_calc(connfd);
+			exit(0);
+		}
+		Close(connfd);				/*parent closes connectd socket*/
+		
+	}
+}
+
+
